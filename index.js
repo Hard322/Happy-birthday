62b2260909202e6871a2b185f4e32e20665a5ebe
Hard322 <494813345@qq.com>
2017-02-28 22:47:53 +0800

    人物动画添加
    animation添加动画

--- index.js
+++ index.js
@@ -1,19 +1,44 @@
-var container=$("#container"),
-	contentWrap=container.find(":first"),
-	eachBg=contentWrap.find("li"),
-	containerWidth=container.width(),
-	containerHeight=container.height();
-contentWrap.css({
-	"width":containerWidth*3+'px',
-	"height":containerHeight+'px'
-});
-eachBg.each(function(){
-	$(this).css({
-		"width":containerWidth+'px',
-	    "height":containerHeight+'px'
+//背景图滑动
+function swipe(container){
+	var contentWrap=container.find(":first"),
+		eachBg=contentWrap.find("li"),
+		containerWidth=container.width(),
+		containerHeight=container.height();
+	contentWrap.css({
+		"width":containerWidth*3+'px',
+		"height":containerHeight+'px'
 	});
-});
-contentWrap.css({
-	"transform":"translate(-"+containerWidth*2+"px,0)",
-	"transition":"transform 5s linear"
+	eachBg.each(function(){
+		$(this).css({
+			"width":containerWidth+'px',
+		    "height":containerHeight+'px'
+		});
+	});
+	swipe.scrollTo=function(x,speed){
+		contentWrap.css({
+			"transform":"translate(-"+x+"px,0)",
+			"transition":"transform "+speed+"s linear"
+		});
+	};
+	return swipe;
+}
+$(function(){
+	var swipebg=swipe($("#container"));
+	//swipebg.scrollTo($("#container").width()*2,5);
+	var getValue=function(className){
+		var $elm=$(''+className);
+		return {
+			height:$elm.height(),
+			top:$elm.position().top
+		};
+	};
+	var pathY=function(){
+		var data=getValue('.a_background_middle');
+		return data.top+data.height/2;
+	}();
+	$("#boy").css({
+		"top":pathY-$("#boy").height()+25+'px'
+	});
+
+
 });
